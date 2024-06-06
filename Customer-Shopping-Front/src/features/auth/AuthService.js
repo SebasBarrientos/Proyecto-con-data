536157434eadefe09c2224976d7015f926232763
AgusCorrales <aguscorrales1997@gmail.com>
2024-06-06 16:43:09 +0200

    Features/auth

---
+++ Customer-Shopping-Front/src/features/auth/AuthService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000";
+
+
+const getAll = async ()=>{
+    const res = await axios.get(API_URL + "")
+    return res.data
+}
+
+
+
+const authService = {
+    getAll
+  };
+
+
+
+
+
+
+export default authService;
