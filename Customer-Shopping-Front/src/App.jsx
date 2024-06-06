1cb88edd2043265f3ff4b9c79ca475edbc2f5a28
jaironf <jaironf.94@gmail.com>
2024-06-06 16:46:35 +0200

    Componente chart creado

--- Customer-Shopping-Front/src/App.jsx
+++ Customer-Shopping-Front/src/App.jsx
@@ -4,6 +4,17 @@ import Header from './component/Header/Header';
 import Home from './component/Home/Home'
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 
+
+const data = [
+  { name: 'A', value: 30 },
+  { name: 'B', value: 80 },
+  { name: 'C', value: 45 },
+  { name: 'D', value: 60 },
+  { name: 'E', value: 20 },
+  { name: 'F', value: 90 },
+  { name: 'G', value: 55 },
+];
+
 function App() {
   
 
@@ -13,7 +24,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/chartes" element={<Chartes/>} />
+        <Route path="/chartes" element={<Chartes  data={data}/>} />
       </Routes>
     </BrowserRouter>
   </div>
--- Customer-Shopping-Front/src/component/Chartes/Chartes.jsx
+++ Customer-Shopping-Front/src/component/Chartes/Chartes.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
 
-const Chartes = () => {
+const Chart = ({ data }) => {
+  const ref = useRef();
+  useEffect(() => {
+    if (!data || !Array.isArray(data)) {
+      console.error('Data must be an array');
+      return;
+    }
+
+    const width = 400;
+    const height = 400;
+    const radius = Math.min(width, height) / 2;
+
+    const svg = d3.select(ref.current)
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2},${height / 2})`);
+
+    const color = d3.scaleOrdinal()
+      .domain(data.map(d => d.name))
+      .range(d3.schemeCategory10);
+
+    const pie = d3.pie()
+      .value(d => d.value)
+      .sort(null);
+
+    const arc = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    svg.selectAll('path')
+      .data(pie(data))
+      .enter()
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', d => color(d.data.name));
+
+    return () => {
+      svg.selectAll('*').remove();
+    };
+  }, [data]);
+
   return (
-    <div>Chartes</div>
-  )
-}
+    <svg ref={ref}></svg>
+  );
+};
 
-export default Chartes
+export default Chart;
